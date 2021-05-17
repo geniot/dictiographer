d5a4a10418101224dc9d073c2eeb0b2a90e31cd0
Vitaly Sazanovich <vitaly.sazanovich@gmail.com>
2021-05-17 19:54:12 +0200

    Redeploying on push.

--- src/main/java/com/dictiographer/controller/GitHubWebhookController.java
+++ src/main/java/com/dictiographer/controller/GitHubWebhookController.java
@@ -18,6 +18,6 @@ public class GitHubWebhookController {
 
     @GetMapping("/welcome")
     public String welcome() {
-        return "welcome redeployment";
+        return "welcome redeployment 2";
     }
 }
