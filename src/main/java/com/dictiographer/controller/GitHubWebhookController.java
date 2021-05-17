027d0d0c89442c9871ca39062dbeac3ea3d3b2f2
Vitaly Sazanovich <vitaly.sazanovich@gmail.com>
2021-05-17 19:38:55 +0200

    Redeploying on push.

--- src/main/java/com/dictiographer/controller/GitHubWebhookController.java
+++ src/main/java/com/dictiographer/controller/GitHubWebhookController.java
@@ -2,12 +2,18 @@ package com.dictiographer.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/github")
 public class GitHubWebhookController {
     @PostMapping("/update")
     public void update(@RequestBody String json) {
-        System.out.println(json);
+        try {
+            Runtime.getRuntime().exec("sudo systemctl restart dictiographer.service");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @GetMapping("/welcome")
